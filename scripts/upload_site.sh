4df5a4d2f077daed856ab076bdde1c25ef0f64fc
Jonathan Diamond <jonathan@pointonenav.com>
2026-01-07 15:00:02 -0800

    Add cache invalidation to upload script.

--- scripts/upload_site.sh
+++ scripts/upload_site.sh
@@ -12,4 +12,6 @@ echo "Injected version $VERSION into HTML files in $BUILD_DIR"
 
 aws s3 sync --profile robopenguin --delete site/ s3://www.robopenguins.com/fatal_core_dump/
 
+aws cloudfront create-invalidation --profile robopenguin --distribution-id E3SR3H7C34DQ6Z --paths "/fatal_core_dump/*"
+
 git reset HEAD --hard
