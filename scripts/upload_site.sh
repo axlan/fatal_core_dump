b32d73cc2597bbbc8369beaae46bd1de5629d0ac
Jonathan Diamond <jonathan@pointonenav.com>
2026-01-07 14:29:48 -0800

    Add version to footer.

--- scripts/upload_site.sh
+++ scripts/upload_site.sh
@@ -1,4 +1,15 @@
 #! /usr/bin/env bash
 set -e
 
+if [ -n "$(git status --porcelain)" ]; then
+    echo "Warning: Git repository is dirty. Aborting."
+    exit 1
+fi
+
+VERSION=$(git describe --tags)
+sed -i "s/@VERSION@/$VERSION/g" site/*.html
+echo "Injected version $VERSION into HTML files in $BUILD_DIR"
+
 aws s3 sync --profile robopenguin --delete site/ s3://www.robopenguins.com/fatal_core_dump/
+
+git reset HEAD --hard
